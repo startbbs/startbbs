88ef17c8fbc3146a773ba6a4b50a0f905abe36e9
startbbs <startbbs@126.com>
2014-03-04 22:01:49 +0800

    内容页中上下贴链接修正

--- themes/default/block/right_cateinfo.php
+++ themes/default/block/right_cateinfo.php
@@ -6,7 +6,7 @@
         <p><?php echo $cate['content'];?></p>
         <a href="<?php echo site_url('forum/flist/'.$cate['cid']);?>" class="btn btn-default btn-sm">此节点</a>
 <a href="<?php echo site_url('forum/add/'.$cate['cid'])?>" class="btn btn-sm btn-success">新建话题</a>
-<a href="<?php echo $content['previous']?>" class="btn btn-default btn-sm">上一贴</a>
-<a href="<?php echo $content['next']?>" class="btn btn-default btn-sm">下一贴</a>
+<a href="<?php echo site_url('forum/view/'.$content['previous'])?>" class="btn btn-default btn-sm">上一贴</a>
+<a href="<?php echo site_url('forum/view/'.$content['next'])?>" class="btn btn-default btn-sm">下一贴</a>
     </div>
 </div>
