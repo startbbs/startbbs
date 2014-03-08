819ae4ecfe3e518079783ecdde7cc0d95ab6ea95
startbbs <startbbs@126.com>
2014-03-08 21:47:23 +0800

    修改版本号为1.1.6

--- app/config/version.php
+++ app/config/version.php
@@ -3,7 +3,7 @@
 $config = array (
   'sys_name' => 'StartBBS',
   'sys_url' => 'http://www.startbbs.com',
-  'sys_version' => 'V1.1.5',
-  'sys_update' => '20140306',
+  'sys_version' => 'V1.1.6',
+  'sys_update' => '20140315',
   'sys_author' => 'QQ858292510',
 );
