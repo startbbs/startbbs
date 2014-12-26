dfaa429e5c8c541d3ec7da9f70a5d6855d2d6ed5
startbbs <startbbs@126.com>
2014-12-26 23:40:58 +0800

    Modify the credit config setting.

--- app/config/userset.php
+++ app/config/userset.php
@@ -5,8 +5,8 @@
 $config = array (
   'credit_start' => '300',//初始积分
   'credit_login' => '2',//登录积分
-  'credit_post' => '-5',//发贴积分
-  'credit_reply' => '-5',//回复积分
+  'credit_post' => '2',//发贴积分
+  'credit_reply' => '1',//回复积分
   'credit_reply_by' => '3',//被回复积分
   'credit_del' => '-10',//被删除积分
   'credit_follow' => '5',//被关注积分
