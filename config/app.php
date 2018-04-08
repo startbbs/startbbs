b8d5cb6b5e2f43b4c3301213cd24d7db923f9342
startbbs <startbbs@126.com>
2018-04-08 20:11:29 +0800

    修正验证码更新错误

--- config/app.php
+++ config/app.php
@@ -116,7 +116,7 @@ return [
     // 异常处理handle类 留空使用 \think\exception\Handle
     'exception_handle'       => '',
     //版本
-    'version' => 'v2.0.0',
+    'version' => 'v2.0.1',
     //发布时间
-    'publish_time' => '20180316',
+    'publish_time' => '20180408',
 ];
